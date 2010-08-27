2568a303b812df56a8969e8c9f93612e7378f853
HSteeb <hs2014@bible2.net>
2010-08-27 20:29:31 +0200

    N: CHANGES, scripts/makeDistrib.sh

---
+++ scripts/makeDistrib.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# makeDistrib.sh -- build ZoteroXmlImporter distribution .zip file
+#   by Helmut Steeb <hs2010 at bible2.net> (insert current year)
+# Working dir must be root folder ZoteroXmlImporter/
+
+zip -r ZoteroXmlImporter-0.9.zip * -x '*.zip'
+
+
