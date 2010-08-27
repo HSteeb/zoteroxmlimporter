72a407be1a354d709e269c6a61ba37fc3b942d88
HSteeb <hs2014@bible2.net>
2010-08-27 20:30:52 +0200

    M: do not pack ~ files into zip

--- scripts/makeDistrib.sh
+++ scripts/makeDistrib.sh
@@ -3,6 +3,6 @@
 #   by Helmut Steeb <hs2010 at bible2.net> (insert current year)
 # Working dir must be root folder ZoteroXmlImporter/
 
-zip -r ZoteroXmlImporter-0.9.zip * -x '*.zip'
+zip -r ZoteroXmlImporter-0.9.zip * -x '*.zip' '*~'
 
 
